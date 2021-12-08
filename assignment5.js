ae3d006c5838eedb575b24b96525b72e81cd1ec7
reetika <reetika.singh05@gmail.com>
2021-12-08 21:31:04 +0530

    added pipeline config

---
+++ assignment5.js
@@ -0,0 +1,23 @@
+// let admin = 9,
+//   fname = 10.5;
+// let fname = "Guvi ";
+// let lname = "geek";
+// let admin = fname + lname;
+// alert(`hello ${admin}`); // "Guvi geek"
+
+// use parseInt to convert string to number
+
+// let a = parseInt(prompt("First number?"));
+// let b = parseInt(prompt("Second number?"));
+// alert(a + b);
+
+// If you run the below scritpt you will get “Code is Blasted”
+
+// #### dictionary or string of numbers or characters compare chracter by character if first character is greater it will not look into other character
+var a = "2" > "12";
+//Don't touch below this
+if (a) {
+  console.log("Code is Blasted");
+} else {
+  console.log("Diffused");
+}
